c4c58e68f1fc380fe823ff5c5248dda0399dad5e
Songyeonji <104824428+Songyeonji@users.noreply.github.com>
2023-08-11 18:06:50 +0900

    리뷰 캐러셀 만들기

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-const colors = [
-    "#3498db", "#e74c3c", "#8e44ad", "#2ecc71", "#f39c12", "#d35400",
-    "#1abc9c", "#3498db", "#9b59b6", "#27ae60", "#f39c12", "#d35400",
-    "#c0392b", "#2980b9", "#16a085", "#34495e", "#f1c40f", "#e67e22"
-];
+document.addEventListener("DOMContentLoaded", () => {
+    const reviews = [
+        {
+            text: "Excellent product! I'm very satisfied.",
+            avatar: "https://images.unsplash.com/photo-1691525737402-e7a447ec2be5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+        },
+        {
+            text: "Fast shipping and great customer service.",
+            avatar: "https://images.unsplash.com/photo-1691155056059-82a814c79240?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=400&q=60"
+        },
+        {
+            text: "The quality exceeded my expectations.",
+            avatar: "https://plus.unsplash.com/premium_photo-1690489323622-6033f11bb1a4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=400&q=60"
+        },
+        {
+            text: "I love it! Will definitely buy again.",
+            avatar: "https://images.unsplash.com/photo-1690975243919-4aa73dc78c81?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"
+        },
+        {
+            text: "Highly recommended. 5 stars!",
+            avatar: "https://images.unsplash.com/photo-1691502090078-d4d26c6decd0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1NXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"
+        }
+    ];
 
+    const reviewText = document.getElementById("review-text");
+    const avatarImage = document.querySelector(".avatar");
+    const showReviewBtn = document.getElementById("btn");
 
-const colorBtn = document.getElementById("btn");
-const colorName = document.getElementById("color");
-
-colorBtn.addEventListener("click", () => {
-  const randomIndex = Math.floor(Math.random() * colors.length);
-  document.body.style.backgroundColor = colors[randomIndex];
-  colorName.textContent = colors[randomIndex];
+    showReviewBtn.addEventListener("click", () => {
+        const randomIndex = Math.floor(Math.random() * reviews.length);
+        reviewText.textContent = reviews[randomIndex].text;
+        avatarImage.src = reviews[randomIndex].avatar;
+    });
 });
