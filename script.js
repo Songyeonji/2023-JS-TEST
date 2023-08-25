ba1328911dfa8f2116e0d61106958101336eb6a0
Songyeonji <104824428+Songyeonji@users.noreply.github.com>
2023-08-25 19:53:25 +0900

    비디오 정지시작 로직

--- script.js
+++ script.js
@@ -1,39 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const menuItems = [
-        { name: "Pancakes", category: "breakfast", icon: "fas fa-coffee" },
-        { name: "Burger", category: "lunch", icon: "fas fa-hamburger" },
-        { name: "Pizza", category: "dinner", icon: "fas fa-pizza-slice" },
-        // Add more menu items
-    ];
+const video = document.getElementById("video");
+const playPauseBtn = document.getElementById("playPauseBtn");
 
-    const menuList = document.querySelector(".menu-list");
-    const filterButtons = document.querySelectorAll(".filter-btn");
-
-    function displayMenuItems(menu) {
-        menuList.innerHTML = "";
-        menu.forEach(item => {
-            const menuItem = document.createElement("div");
-            menuItem.classList.add("menu-item");
-            menuItem.textContent = item.name;
-            menuList.appendChild(menuItem);
-        });
+playPauseBtn.addEventListener("click", () => {
+    if (video.paused) {
+        video.play();
+        playPauseBtn.innerHTML = '<i class="fas fa-pause"></i>';
+    } else {
+        video.pause();
+        playPauseBtn.innerHTML = '<i class="fas fa-play"></i>';
     }
-
-    function filterMenuItems(category) {
-        if (category === "all") {
-            displayMenuItems(menuItems);
-        } else {
-            const filteredItems = menuItems.filter(item => item.category === category);
-            displayMenuItems(filteredItems);
-        }
-    }
-
-    filterButtons.forEach(button => {
-        button.addEventListener("click", () => {
-            const category = button.dataset.category;
-            filterMenuItems(category);
-        });
-    });
-
-    displayMenuItems(menuItems);
 });
