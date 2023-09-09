e6e45e90947b39abb4bb2f668edaf20d53ca6d8e
Songyeonji <104824428+Songyeonji@users.noreply.github.com>
2023-09-09 20:21:36 +0900

    사이먼게임 기본로직

--- script.js
+++ script.js
@@ -1,37 +1,76 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const choices = document.querySelectorAll(".choice");
-    const result = document.querySelector(".result");
+    const sequenceDisplay = document.getElementById("sequence");
+    const startBtn = document.getElementById("startBtn");
+    const resetBtn = document.getElementById("resetBtn");
 
-    choices.forEach(choice => {
-        choice.addEventListener("click", () => {
-            const playerChoice = choice.getAttribute("data-choice");
-            const computerChoice = getComputerChoice();
-            const gameResult = determineWinner(playerChoice, computerChoice);
+    const colors = ["red", "green", "blue", "yellow"];
+    const sequence = [];
+    let playerTurn = false;
+    let currentIndex = 0;
 
-            result.textContent = `You chose ${playerChoice}. Computer chose ${computerChoice}. ${gameResult}`;
-        });
+    startBtn.addEventListener("click", startGame);
+    resetBtn.addEventListener("click", resetGame);
+
+    function startGame() {
+        startBtn.setAttribute("disabled", "true");
+        resetBtn.removeAttribute("disabled");
+        generateNextMove();
+    }
+
+    function resetGame() {
+        sequence.length = 0;
+        playerTurn = false;
+        currentIndex = 0;
+        sequenceDisplay.textContent = "";
+        startBtn.removeAttribute("disabled");
+        resetBtn.setAttribute("disabled", "true");
+    }
+
+    function generateNextMove() {
+        const randomColor = colors[Math.floor(Math.random() * colors.length)];
+        sequence.push(randomColor);
+        playSequence();
+    }
+
+    function playSequence() {
+        let i = 0;
+        const interval = setInterval(() => {
+            highlightColor(sequence[i]);
+            i++;
+            if (i >= sequence.length) {
+                clearInterval(interval);
+                playerTurn = true;
+                currentIndex = 0;
+            }
+        }, 1000);
+    }
+    function highlightColor(color) {
+        const coloredDiv = document.createElement("div");
+        coloredDiv.style.backgroundColor = color;
+        coloredDiv.className = "highlighted"; // 수정: 클래스 이름을 "highlighted"로 변경
+        sequenceDisplay.appendChild(coloredDiv);
+        setTimeout(() => {
+            coloredDiv.style.backgroundColor = "transparent";
+        }, 500);
+    }
+    
+
+    sequenceDisplay.addEventListener("click", (event) => {
+        if (playerTurn) {
+            const clickedColor = event.target.style.backgroundColor;
+            const expectedColor = sequence[currentIndex];
+            if (clickedColor === expectedColor) {
+                currentIndex++;
+                if (currentIndex >= sequence.length) {
+                    playerTurn = false;
+                    setTimeout(() => {
+                        generateNextMove();
+                    }, 1000);
+                }
+            } else {
+                alert("Wrong sequence! Game over.");
+                resetGame();
+            }
+        }
     });
-
-    function getComputerChoice() {
-        const choices = ["rock", "paper", "scissors"];
-        const randomIndex = Math.floor(Math.random() * choices.length);
-        return choices[randomIndex];
-    }
-
-    function determineWinner(playerChoice, computerChoice) {
-        if (playerChoice === computerChoice) {
-            return "It's a tie!";
-        }
-
-        switch (playerChoice) {
-            case "rock":
-                return computerChoice === "scissors" ? "You win!" : "Computer wins!";
-            case "paper":
-                return computerChoice === "rock" ? "You win!" : "Computer wins!";
-            case "scissors":
-                return computerChoice === "paper" ? "You win!" : "Computer wins!";
-            default:
-                return "Invalid choice";
-        }
-    }
 });
