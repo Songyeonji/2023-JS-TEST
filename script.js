a46d4d62cc2c3055516007392c7c756422522965
Songyeonji <104824428+Songyeonji@users.noreply.github.com>
2023-09-18 16:58:17 +0900

    캐릭터 추가 및 발판 로직추가

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const player = document.getElementById("player");
 const gameContainer = document.getElementById("gameContainer");
 let isJumping = false;
+let jumpHeight = 0;
+let jumpDirection = 1;
 
 player.addEventListener("click", () => {
     if (!isJumping) {
@@ -10,26 +12,19 @@ player.addEventListener("click", () => {
 
 function jump() {
     isJumping = true;
-    let jumpHeight = 0;
     const jumpInterval = setInterval(() => {
-        if (jumpHeight < 100) { // 최대 점프 높이
-            jumpHeight += 5; // 점프 속도 및 높이 조절 가능
+        if (jumpHeight < 150 && jumpDirection === 1) {
+            jumpHeight += 5;
+            player.style.bottom = jumpHeight + "px";
+        } else {
+            jumpDirection = -1;
+        }
+
+        if (jumpHeight > 0 && jumpDirection === -1) {
+            jumpHeight -= 5;
             player.style.bottom = jumpHeight + "px";
         } else {
             clearInterval(jumpInterval);
-            fall();
-        }
-    }, 20);
-}
-
-function fall() {
-    const fallInterval = setInterval(() => {
-        let currentPosition = parseInt(player.style.bottom);
-        if (currentPosition > 0) { // 땅에 닿을 때까지 떨어지기
-            currentPosition -= 5; // 낙하 속도 조절 가능
-            player.style.bottom = currentPosition + "px";
-        } else {
-            clearInterval(fallInterval);
             isJumping = false;
         }
     }, 20);
