b7c394eaf08acaac95a15ac5cebf93efd494172b
Songyeonji <104824428+Songyeonji@users.noreply.github.com>
2023-09-22 17:54:40 +0900

    두더지 게임 기본로직

--- script.js
+++ script.js
@@ -1,121 +1,56 @@
-const player = document.getElementById("player");
-const gameContainer = document.getElementById("gameContainer");
-const platforms = document.querySelectorAll(".platform"); // 발판 선택
-let isJumping = false;
-let jumpHeight = 0;
-let jumpDirection = 1;
-let isFalling = false;
-let platformBottom = 100; // 초기 플랫폼의 아래 위치
+const holes = document.querySelectorAll('.hole');
+const scoreDisplay = document.querySelector('.score');
+const timeDisplay = document.querySelector('.time');
+const startButton = document.querySelector('.start-button');
 
-// Enter 키로 점프 활성화
-document.addEventListener("keydown", (event) => {
-    if (event.key === "Enter" && !isJumping && !isFalling) {
-        jump();
-    }
-});
+let score = 0;
+let timeLeft = 60;
+let isGameRunning = false;
 
-document.addEventListener("keydown", (event) => {
-    if (event.key === "ArrowLeft") {
-        moveLeft();
-    } else if (event.key === "ArrowRight") {
-        moveRight();
-    }
-});
-
-
-function jump() {
-    isJumping = true;
-    const jumpInterval = setInterval(() => {
-        if (jumpHeight < 200 && jumpDirection === 1) {
-            jumpHeight += 5;
-            player.style.bottom = jumpHeight + "px";
-        } else {
-            jumpDirection = -1;
-        }
-
-        if (jumpHeight > 0 && jumpDirection === -1) {
-            jumpHeight -= 5;
-            player.style.bottom = jumpHeight + "px";
-        } else {
-            clearInterval(jumpInterval);
-            isJumping = false;
-            isFalling = true;
-            fall();
-        }
-    }, 20);
-}
-
-function fall() {
-    const fallInterval = setInterval(() => {
-        let currentPosition = parseInt(player.style.bottom);
-        if (currentPosition > 0) {
-            currentPosition -= 5;
-            player.style.bottom = currentPosition + "px";
-        } else {
-            clearInterval(fallInterval);
-            isFalling = false;
-        }
-    }, 20);
-}
-
-function moveLeft() {
-    const currentLeft = parseInt(player.style.left) || 0;
-    const newLeft = currentLeft - 10;
-    player.style.left = newLeft + "px";
-}
-
-function moveRight() {
-    const currentLeft = parseInt(player.style.left) || 0;
-    const newLeft = currentLeft + 10;
-    player.style.left = newLeft + "px";
-}
-
-
-// 플랫폼을 생성하는 함수
-function createPlatform() {
-    const platform = document.createElement("div");
-    platform.classList.add("platform");
-    platform.style.bottom = platformBottom + "px";
-    platform.style.left = Math.random() * (gameContainer.offsetWidth - 50) + "px"; // 랜덤한 가로 위치
-    gameContainer.appendChild(platform);
-    platformBottom += 100; // 다음 플랫폼의 아래 위치
-}
-
-
-// 발판 충돌 감지 및 떨어지기 처리
-function checkPlatformCollision() {
-    platforms.forEach((platform) => {
-        const playerRect = player.getBoundingClientRect();
-        const platformRect = platform.getBoundingClientRect();
-
-        if (
-            playerRect.bottom >= platformRect.top &&
-            playerRect.bottom <= platformRect.bottom &&
-            playerRect.right >= platformRect.left &&
-            playerRect.left <= platformRect.right
-        ) {
-            // 플랫폼을 밟았을 때
-            jumpHeight = 0;
-            player.style.bottom = jumpHeight + "px";
-            isFalling = false;
-        } else {
-            // 플랫폼을 밟지 않았을 때
-            if (!isJumping) {
-                isFalling = true;
-                fall();
+function startGame() {
+    if (!isGameRunning) {
+        isGameRunning = true;
+        score = 0;
+        timeLeft = 60;
+        scoreDisplay.textContent = `Score: ${score}`;
+        timeDisplay.textContent = `Time: ${timeLeft}`;
+        startButton.disabled = true;
+        moleUp();
+        const countdown = setInterval(() => {
+            timeLeft--;
+            timeDisplay.textContent = `Time: ${timeLeft}`;
+            if (timeLeft <= 0) {
+                clearInterval(countdown);
+                endGame();
             }
-        }
-    });
+        }, 1000);
+    }
 }
 
-    // 플랫폼이 화면 위로 사라지면 새로운 플랫폼 생성
-    const topPlatform = document.querySelector(".platform");
-    if (topPlatform && topPlatform.getBoundingClientRect().bottom <= 0) {
-        gameContainer.removeChild(topPlatform);
-        createPlatform();
-    }
+function endGame() {
+    isGameRunning = false;
+    startButton.disabled = false;
+    alert(`Game Over! Your score is ${score}`);
+}
 
-// 주기적으로 충돌 감지 및 플랫폼 관리
-setInterval(() => {
-    checkPlatformCollision();
-}, 20);
+function moleUp() {
+    const randomTime = Math.random() * 2000 + 500; // 0.5초 ~ 2.5초
+    const randomHole = holes[Math.floor(Math.random() * holes.length)];
+    randomHole.classList.add('up');
+    setTimeout(() => {
+        randomHole.classList.remove('up');
+        if (isGameRunning) {
+            moleUp();
+        }
+    }, randomTime);
+}
+
+function bonk(e) {
+    if (!e.isTrusted) return; // 가짜 클릭을 방지
+    score++;
+    this.classList.remove('up');
+    scoreDisplay.textContent = `Score: ${score}`;
+}
+
+holes.forEach(hole => hole.addEventListener('click', bonk));
+startButton.addEventListener('click', startGame);
