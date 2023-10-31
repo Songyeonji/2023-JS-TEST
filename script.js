61e545fee42ec24a3fc7fc91bd7c1766c7533daf
Songyeonji <104824428+Songyeonji@users.noreply.github.com>
2023-10-31 17:48:27 +0900

    바닐라 자바스크립트 간단한 계산기

--- script.js
+++ script.js
@@ -1,116 +1,24 @@
-const calendar = document.getElementById('calendar');
-const calendarTitle = document.getElementById('calendarTitle');
-const prevMonthButton = document.getElementById('prevMonth');
-const nextMonthButton = document.getElementById('nextMonth');
-const memoContainer = document.querySelector('.memo-container');
-const selectedDateElement = document.getElementById('selectedDate');
-const memoText = document.getElementById('memoText');
-const saveMemoButton = document.getElementById('saveMemo');
+document.addEventListener("DOMContentLoaded", function () {
+    const display = document.getElementById("display");
+    const buttons = document.querySelectorAll(".number, .operator");
 
-let currentYear, currentMonth;
-let selectedDate;
-let memos = {};
-
-prevMonthButton.addEventListener('click', () => {
-    currentMonth--;
-    if (currentMonth < 0) {
-        currentMonth = 11;
-        currentYear--;
-    }
-    createCalendar(currentYear, currentMonth);
-});
-
-nextMonthButton.addEventListener('click', () => {
-    currentMonth++;
-    if (currentMonth > 11) {
-        currentMonth = 0;
-        currentYear++;
-    }
-    createCalendar(currentYear, currentMonth);
-});
-
-saveMemoButton.addEventListener('click', () => {
-    const memo = memoText.value;
-    const date = new Date(currentYear, currentMonth, selectedDate);
-    memos[date.toDateString()] = memo;
-    memoContainer.style.display = 'none';
-    createCalendar(currentYear, currentMonth);
-});
-
-function createCalendar(year, month) {
-    const daysInMonth = new Date(year, month + 1, 0).getDate();
-    const firstDayOfMonth = new Date(year, month, 1).getDay();
-
-    const monthNames = [
-        'January', 'February', 'March', 'April',
-        'May', 'June', 'July', 'August',
-        'September', 'October', 'November', 'December'
-    ];
-
-    const table = document.createElement('table');
-    const caption = document.createElement('caption');
-    caption.textContent = `${monthNames[month]} ${year}`;
-    table.appendChild(caption);
-
-    const thead = document.createElement('thead');
-    const tr = document.createElement('tr');
-
-    const dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-    dayNames.forEach(dayName => {
-        const th = document.createElement('th');
-        th.textContent = dayName;
-        tr.appendChild(th);
-    });
-
-    thead.appendChild(tr);
-    table.appendChild(thead);
-    currentYear = year;
-    currentMonth = month;
-
-    const tbody = document.createElement('tbody');
-    let day = 1;
-    for (let i = 0; i < 6; i++) {
-        const tr = document.createElement('tr');
-        for (let j = 0; j < 7; j++) {
-            if (i === 0 && j < firstDayOfMonth) {
-                const td = document.createElement('td');
-                tr.appendChild(td);
-            } else if (day <= daysInMonth) {
-                const td = document.createElement('td');
-                const dateButton = document.createElement('button');
-                dateButton.textContent = day;
-                dateButton.setAttribute('data-date', `${year}-${month + 1}-${day}`);
-                td.appendChild(dateButton);
-                tr.appendChild(td);
-                day++;
-            }
-        }
-        tbody.appendChild(tr);
-    }
-
-    table.appendChild(tbody);
-    calendar.innerHTML = '';
-    calendar.appendChild(table);
-
-    const dateButtons = Array.from(document.querySelectorAll('button[data-date]'));
-    dateButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            const date = button.getAttribute('data-date');
-            if (date) {
-                selectedDate = date;
-                displayMemo(selectedDate);
+    buttons.forEach((button) => {
+        button.addEventListener("click", function () {
+            if (button.value === "=") {
+                try {
+                    display.value = eval(display.value);
+                } catch (error) {
+                    display.value = "Error";
+                }
+            } else if (button.value === "C") {
+                display.value = "";
+            } else if (button.value === "sqrt") {
+                display.value = Math.sqrt(eval(display.value));
+            } else if (button.value === "^") {
+                display.value += "**";
+            } else {
+                display.value += button.value;
             }
         });
     });
-}
-
-function displayMemo(date) {
-    selectedDateElement.textContent = `Memo for ${date}`;
-    memoText.textContent = memos[date] || '';
-    memoContainer.style.display = 'block';
-}
-
-const today = new Date();
-currentYear = today.getFullYear();
-currentMonth = today.getMonth();
-createCalendar(currentYear, currentMonth);
+});
